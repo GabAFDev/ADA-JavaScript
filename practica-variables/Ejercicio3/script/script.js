61af23277eef1c8e7584095aadb05a176a5c34e8
Gabriela Figueroa <gabfigueroa@icloud.com>
2023-02-24 06:55:39 -0300

    adding practica-funciones

---
+++ practica-variables/Ejercicio3/script/script.js
@@ -0,0 +1,11 @@
+let user = "Gabriela"
+
+let age = "27"
+
+let email = "gabfigueroa@icloud.com"
+
+console.log(`Hola ${user}, tenés ${age} y tu medio de contacto es ${email}`)
+
+document.write(`Hola ${user}, tenés ${age} y tu medio de contacto es ${email}`)
+
+alert(`Hola ${user}, tenés ${age} y tu medio de contacto es ${email}`)
---
+++ practica-variables/Ejercicio4/script/script.js
@@ -0,0 +1,9 @@
+let num1 = Number(prompt("Ingrese un número:"))
+
+let num2 = Number(prompt("Ingrese otro número:"))
+
+let suma = num1 + num2
+
+console.log(`El resultado de la suma es: ${suma}`)
+
+document.write(`El resultado de la suma es: ${suma}`)
