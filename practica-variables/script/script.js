b5cb221c290a3e5b5cf2fcc3f79c515e5f45fcd2
Gabriela Figueroa <gabfigueroa@icloud.com>
2023-02-24 05:31:38 -0300

    adding practica-varibles

---
+++ practica-variables/script/script.js
@@ -0,0 +1,11 @@
+let user = "Gabriela"
+console.log(user)
+console.log(typeof(user))
+
+let age = "27"
+console.log(age)
+console.log(typeof(age))
+
+let email = "gabfigueroa@icloud.com"
+console.log(email)
+console.log(typeof(email))
