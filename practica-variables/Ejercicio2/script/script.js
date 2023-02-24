0148b739e689efe34a24d1e477b5939922df1e6b
Gabriela Figueroa <gabfigueroa@icloud.com>
2023-02-24 06:07:32 -0300

    adding ejercicio2

---
+++ practica-variables/Ejercicio2/script/script.js
@@ -0,0 +1,13 @@
+let name = prompt("Ingrese su nombre:")
+
+let age = Number(prompt("Ingrese su edad:"))
+
+let country = prompt("Ingrese su país de origen:")
+
+let job = prompt("Ingrese su profesión:")
+
+console.log(`Te llamas ${name}, tenes ${age}, sos de ${country} y trabajas de ${job}`)
+
+let age10 = age + 10
+
+console.log(age10)
